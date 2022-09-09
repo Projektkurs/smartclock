fc8890a5dda82b38350260da215c135e71992c8b
Ben Mattes Krusekamp <ben.krause05@gmail.com>
2022-09-09 21:47:26 +0200

    fix to last commit (addition of example.dart

---
+++ lib/components/example.dart
@@ -0,0 +1,39 @@
+/* example.dart - example to build a barebones component
+ *
+ * Copyright 2022 by Ben Mattes Krusekamp <ben.krause05@gmail.com>
+ */
+
+import 'package:smartclock/main_header.dart';
+
+class ExampleComponent extends Component 
+{
+  
+  @override
+  ExampleComponent(
+    {required Key key,
+    required this.gconfig,
+    required this.resizeWidget,
+    required this.configMenu,
+    })
+    : super(key: key,gconfig:gconfig,configMenu:configMenu);
+  final Function configMenu;
+
+  final GeneralConfig<EmptyComponentConfig> gconfig;
+  final Function resizeWidget;
+  @override
+  ExampleComponentState createState() => ExampleComponentState();
+}
+
+class ExampleComponentState extends State<ExampleComponent> with ComponentBuild<ExampleComponent>
+{
+  @override
+  Widget build(BuildContext context) {
+    if(firstbuild){
+      defaultfirstbuild();
+    }
+    return component_build(Container(
+      color: Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0),
+      child:const SizedBox.expand())
+    );
+  }
+}
